aedfafe712c527ea9cf4438c6c18cfafe00cca62
maram <maram.hassan95@gmail.com>
2023-09-07 09:38:48 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main (void){
-puts("Programming is like building a multilingual puzzle");
-return 0;
+
+int main(void) {
+    // Print a "test" message and add a newline character
+    puts("test");
+
+    // Return 0 to indicate successful execution
+    return 0;
 }
+
