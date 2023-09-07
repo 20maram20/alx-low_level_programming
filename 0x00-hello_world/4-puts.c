742e0caa398f1736cf430b41be5010cad1553a26
maram <maram.hassan95@gmail.com>
2023-09-07 10:39:39 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-/*use puts function*/
+/**
+ * main-entry point
+ *
+ * use puts function
+ *
+ *return 0 (success)
+ * */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
