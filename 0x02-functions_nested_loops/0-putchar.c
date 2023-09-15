a363ceb7046cfcc2c47a8110b2068b04dbdbfe3e
maram <maram.hassan95@gmail.com>
2023-09-15 08:47:43 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "unistd.h"
 /**
 *main - is the entry point for the program
 *Description: Prints _putchar using putchar prototype
@@ -7,10 +8,9 @@
 int main(void)
 {
 	char str[] = "_putchar";
-	int ch;
 
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
+	for (int ch = 0; ch < 8; ch++)
+	_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
