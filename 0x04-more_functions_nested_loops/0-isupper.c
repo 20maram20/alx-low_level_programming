29d4e7ff70d74a57217e330e7399380d53dcaaa0
maram <maram.hassan95@gmail.com>
2023-09-15 05:05:17 -0700

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper fumction -  check if c is upper case
+ * @c:is input for function
+ * Return: 1 if upper case and 0 if not.
+ *
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+else
+return (0);
+}
+}
